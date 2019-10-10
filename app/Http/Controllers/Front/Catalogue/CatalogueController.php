67470b97264de71009d6dda5a17d9d61a5fcd7b3
mac <shahogjalal@gmail.com>
2019-10-11 01:11:05 +0500

    changes

--- app/Http/Controllers/Front/Catalogue/CatalogueController.php
+++ app/Http/Controllers/Front/Catalogue/CatalogueController.php
@@ -45,7 +45,7 @@ class CatalogueController extends Controller
         if($size) {
             $query->whereIn('products.attribute_value_size_id', $size);
         }
-        $query->where('products.sizing_gender', $sizing_gender)->orWhere('products.sizing_gender', 'NONE');
+        $query->where('products.sizing_gender', $sizing_gender);
 
         $query->where('products.status', 1);
         
